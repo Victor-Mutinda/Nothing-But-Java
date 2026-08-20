d935856d5fcb5fbad9349a745b196a3d0aee9ed6
victor mutinda <victormutinda01@gmail.com>
2026-08-20 23:16:23 +0300

    Nothing much

--- rightAngledTriangleLeft.java
+++ rightAngledTriangleLeft.java
@@ -1,22 +1,23 @@
 public class rightAngledTriangleLeft {
     public static void main(String[] args){
         int rows = 5 ;
-        int k = 1 ;
-        int l = 5 ;
+        int k ;
+        int l ;
 
     // Left sided Right Angled Triangle
         for(k = 0; k < rows; k++){
+            
             for(l = 2 * (5 - k); l >= 0; l--){
-
                 System.out.print(" ");
-                
+               // System.out.print("Y");   
             }
+
             for(l = 0; l <=k; l++){
                  System.out.print("* ");
-
             }
             
             System.out.println();
         }
-}
+
+    }
 }
