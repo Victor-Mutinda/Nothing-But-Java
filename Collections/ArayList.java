49887e250332c2d4fc1886e327120c921c52e45e
victor mutinda <victormutinda01@gmail.com>
2026-09-22 23:59:10 +0300

    Collections in Java - ArrayLists

---
+++ Collections/ArayList.java
@@ -0,0 +1,41 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class ArayList {
+    public static void main(String[] args){
+// Creating an Array List
+        ArrayList<String> pets = new ArrayList<>();
+// Add items in a list.
+        pets.add("Sparky");
+        pets.add("Sting");
+        pets.add("Kamai");
+        pets.add("Bob");
+        pets.add("Santa");
+        pets.add("Serena");
+        pets.add("Rafiki");
+        pets.add("Ishmael");
+        pets.add("Ishamael");
+        pets.add("Lola");
+
+        System.out.println("List of all my Dog Pets since 2010 till to date:");
+        System.out.println(pets);
+
+// Add an item at a specific index position
+        pets.add(10, "Shepherd");
+        System.out.println(pets);
+
+// Removing an item at a specific index position
+        pets.remove(8);
+         System.out.println(pets);
+// Removing an item without using index position
+        pets.remove("Ishmael");
+        System.out.println(pets);
+
+// Updating an item at a index position
+        pets.set(0, "Sparky The great");
+        System.out.println(pets);
+
+    }
+    
+}
